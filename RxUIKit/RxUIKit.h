5cc61159321e7288a9eb0931c9af06ca589b3b88
Alexander Naumov <alexander@imac-alexander.local>
2018-10-19 17:38:57 +0300

    Initial Commit

---
+++ RxUIKit/RxUIKit.h
@@ -0,0 +1,19 @@
+//
+//  RxUIKit.h
+//  RxUIKit
+//
+//  Created by Alexander Naumov on 19/10/2018.
+//  Copyright © 2018 Alexander Naumov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxUIKit.
+FOUNDATION_EXPORT double RxUIKitVersionNumber;
+
+//! Project version string for RxUIKit.
+FOUNDATION_EXPORT const unsigned char RxUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxUIKit/PublicHeader.h>
+
+
