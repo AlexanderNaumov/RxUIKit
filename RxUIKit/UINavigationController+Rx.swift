602ef43a8a7d93ee86b216bbbcc451a7ae8d5ea2
Alexander Naumov <alexander@iMac-Alexander.local>
2018-12-05 14:08:32 +0300

    set navigation delegate

---
+++ RxUIKit/UINavigationController+Rx.swift
@@ -0,0 +1,26 @@
+//
+//  UINavigationController+Rx.swift
+//  RxUIKit
+//
+//  Created by Alexander Naumov on 05/12/2018.
+//  Copyright © 2018 Alexander Naumov. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+extension Reactive where Base: UINavigationController {
+    public func setDelegate(_ delegate: UINavigationControllerDelegate) -> Disposable {
+        return RxNavigationControllerDelegateProxy.installForwardDelegate(delegate, retainDelegate: false, onProxyForObject: base)
+    }
+    
+    public func setRetainDelegate(_ delegate: UINavigationControllerDelegate) -> Disposable {
+        return RxNavigationControllerDelegateProxy.installForwardDelegate(delegate, retainDelegate: true, onProxyForObject: base)
+    }
+    public func setRetainDelegates(_ delegates: [UINavigationControllerDelegate]) -> Disposable {
+        let delegate = MultipleDelegate<UINavigationControllerDelegate>()!
+        delegates.forEach(delegate.addDelegate)
+        return setRetainDelegate(delegate.delegate)
+    }
+}
